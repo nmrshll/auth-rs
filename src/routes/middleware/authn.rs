d15b2f276d73459a71b6da855bae1532fae5a593
Nicolas Marshall <marshall.nicolas@gmail.com>
2019-11-27 18:20:34 +0100

    cleanup

--- src/routes/middleware/authn.rs
+++ src/routes/middleware/authn.rs
@@ -5,50 +5,10 @@ use crate::errors::ServiceError;
 use crate::utils::pass_hash::AuthnToken;
 
 pub fn tokenAuthn(req: &Request<Body>) -> Result<AuthnToken, ServiceError> {
-    let cookies = req.headers().typed_get::<Cookie>().expect("oh no");
-    // dbg!(cookies.get("adminer_sid"));
-    // let optTokenKV = match req.headers().get("cookie") {
-    //     None => None,
-    //     Some(headerValues) => {
-    //         headerValues
-    //             .to_str()?
-    //             .split("; ")
-    //             .map(|headerKV| headerKV.splitn(2, "=").collect::<Vec<&str>>())
-    //             // if split.len() != 2 {
-    //             //     return Err(ServiceError::new_bad_request("invalid cookie header"));
-    //             // }
-    //             // split[0] == "token"
-    //             .find(|kvSplit| kvSplit[0] == "token")
-    //         //
-    //         // .map(|i| Ok((split[0], split[1])))
-    //         // .take_while(Result::is_ok)
-    //         // .map(Result::unwrap)
-    //         // .find(|(key, _)| *key == "token");
-    //         // .take(1);
-    //         // .collect::<Vec<&str>>();
-    //     }
-    // };
-    // match optTokenKV {
-    //     None => return Err(ServiceError::Unauthorized),
-    //     Some(tokenKV) => {
-    //         if tokenKV.len() != 2 {
-    //             return Err(ServiceError::new_bad_request("invalid cookie header"));
-    //         }
-    //     }
-    // };
-    // let optToken = match req.headers().get("cookie") {
-    //     Some(headerValue) => {
-    //         dbg!(&headerValue);
-    //         let valueSplit = headerValue
-    //             .to_str()
-    //             .unwrap()
-    //             .split("=")
-    //             .collect::<Vec<&str>>();
-    //         let rhs = valueSplit.get(1).unwrap().to_owned();
-    //         Some(rhs)
-    //     }
-    //     None => None,
-    // };
+    let cookies = match req.headers().typed_get::<Cookie>() {
+        None => return Err(ServiceError::Unauthorized),
+        Some(cookies) => cookies,
+    };
     match cookies.get("token") {
         Some(token_str) => {
             dbg!(&token_str);
